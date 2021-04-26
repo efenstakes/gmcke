dc0a2fb27dfde791b908fcfb56765f5d0b6edf18
Efen Stakes <efenstakes101@gmail.com>
2021-04-26 21:58:02 +0300

    add challenge card component

---
+++ src/components/challenge_card/challenge_card.component.js
@@ -0,0 +1,132 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+
+import { Grid, Chip } from '@material-ui/core'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+
+import LinkButtonComponent from '../buttons/link_button.component'
+import VSpacerComponent from '../v_spacer/v_spacer.component'
+
+import './challenge_card.component.scss'
+
+
+const ChallengeCardComponent = ({ challenge, isAlt=false }) => {
+    return (
+        <div id={`#{challenge.title}`} className={
+            clsx({
+                "challenge_card": true,
+                "challenge_card___not_alt": !isAlt,
+            })
+        }>
+
+            <Grid container className={
+                clsx({
+                    "challenge_card__grid": true,
+                    "challenge_card__grid__alt": isAlt,
+                })
+            }>
+                {/* image */}
+                <Grid item sm={12} md={6}>
+                <div className={
+                    clsx({
+                        "challenge_card__image_wrapper": true,
+                        "challenge_card__image_wrapper__not_alt": !isAlt
+                    })
+                }>
+                    <img 
+                        src={challenge.imageUrl}
+                        className="challenge_card__image_wrapper__image"
+                    />
+                </div>
+                </Grid>
+
+                {/* content */}
+                <Grid item sm={12} md={6}>
+                <div className="challenge_card__content">
+
+                    <h3>
+                        { challenge.title }
+                    </h3>
+
+                    <p>
+                        { challenge.text1 }
+                    </p>
+                    <p>
+                        { challenge.text2 }
+                    </p>
+
+                    <VSpacerComponent space={2} />
+
+                    <h4>
+                        Made For
+                    </h4>
+                    <VSpacerComponent space={1} />
+                    <div className="challenge_card__content__tags">
+                        {
+                            challenge.made_for.map((tg, index)=> {
+                                return (
+                                    <MadeForChipComponent
+                                        key={index}
+                                        text={tg}
+                                        label={tg}
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                    
+                    <VSpacerComponent space={3} />
+
+                    <LinkButtonComponent
+                        to={`/apply?type=${challenge.title}`}
+                        text="Apply"
+                        endIcon={
+                            <ArrowForwardIosIcon
+                                style={{
+                                    fontSize: '.8rem',
+                                }}
+                            />
+                        }
+                        styles={{
+                            minWidth: '200px',
+                        }}
+                    />
+
+                </div>
+                </Grid>
+            </Grid>
+            
+        </div>
+    )
+}
+
+ChallengeCardComponent.propTypes = {
+    challenge: PropTypes.object.isRequired,
+    isAlt: PropTypes.bool,
+}
+
+
+const MadeForChipComponent = ({ text })=> {
+
+    return (
+        <div style={{
+            display: 'block',
+            marginBottom: '10px',
+            marginRight: '10px',
+        }}>
+            <Chip
+                color="primary"
+                label={text}
+                size="small"
+                style={{
+                    padding: '4px 12px',
+                }}
+            />
+        </div>
+    )
+}
+
+
+export default ChallengeCardComponent
